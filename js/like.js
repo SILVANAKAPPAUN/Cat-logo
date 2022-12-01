48f061f5d1ed667cbae9d12ea2834e4d2a7d0b53
silvana <silvana.k@unochapeco.edu.br>
2022-12-01 15:07:07 -0300

    first-commit

---
+++ js/like.js
@@ -0,0 +1,8 @@
+var count = 0;
+    var btn = document.getElementById("btn-like");
+    var disp = document.getElementById("contador");
+
+function likecounter(){
+        count++;
+        disp.innerHTML = count;
+}
